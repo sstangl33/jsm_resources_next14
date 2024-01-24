ef36e5ab5e3f2e12ac1364ce6cbfded1a587df7e
Stephen Stangl <stevestangl@gmail.com>
2024-01-24 12:22:01 -0500

    Fix Skelteton, improve metadata, and add favicon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "JS Mastery",
   description: "JS Mastery Resources",
+  other: {
+    'theme-color': '#0d1117',
+    'color-scheme': 'dark only',
+    'twitter:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    'twitter-card': 'summary_large_image',
+    'og:url': 'https://jsm-resources-next14.vercel.app/',
+    'og:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    'og:type': 'website',
+  }
 };
 
 export default function RootLayout({
